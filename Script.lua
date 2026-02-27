8a04a5154817f30339eda30621edb7144f75c6a4
ADSKerOffical <137511182+ADSKerOffical@users.noreply.github.com>
2026-02-27 16:18:00 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,2 +1,2 @@
 restorefunction(getrawmetatable(game)["__namecall"])
-task.delay(0.2, newcclosure(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/ADSKerOffical/BHU2Project/refs/heads/main/Hubs/120329833999324"))() end))
+task.delay(task.wait(), newcclosure(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/ADSKerOffical/BHU2Project/refs/heads/main/Hubs/120329833999324"))() end))
