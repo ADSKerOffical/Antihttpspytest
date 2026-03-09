a75a7dc4309eee28c0859c26bf8e3aedcb98583b
ADSKerOffical <137511182+ADSKerOffical@users.noreply.github.com>
2026-03-09 23:20:35 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,13 @@
-print(debug.getinfo(2).name)
+for i = 0, math.huge do
+  local suc, _ = pcall(function()
+    return debug.getinfo(i)
+  end)
+  if suc then
+    print(tostring(i) .. " stack:")
+    for i, v in next, debug.getinfo(i) do
+      print(i, v)
+    end
+  else
+    break
+  end
+end
